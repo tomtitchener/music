a87a0aba397b79bcb7015068f4743920c3fa6a29
ttitchener <ttitchener@gmail.com>
2020-04-17 21:02:59 -0400

    lilypond compliance

--- src/Note.hs
+++ src/Note.hs
@@ -12,14 +12,14 @@ import Pitch
 import Utils
 
 -- PercussionInstrumentNote e.g. "cl" == ClavesNote
-data Note = Note { _notePitch :: Pitch, _noteoOctave :: Octave, _noteDuration :: Duration, _notedDynamic :: Dynamic, _noteAccent :: Accent, _noteSlur :: Bool }
+data Note = Note { _notePitch :: Pitch, _noteoOctave :: Octave, _noteDuration :: Duration, _noteAccent :: Accent, _notedDynamic :: Dynamic, _noteSlur :: Bool }
   deriving (Eq, Ord, Show)
 
 instance ToLily Note where
-  toLily (Note pit oct dur dyn acc slr) = toLily pit <> toLily oct <> toLily dur <> toLily dyn <> toLily acc <> if slr then "~" else ""
+  toLily (Note pit oct dur acc dyn slr) = toLily pit <> toLily oct <> toLily dur <> toLily acc <> toLily dyn <> if slr then "~" else ""
 
 parseNote :: Parser Note
-parseNote = Note <$> parsePitch <*> parseOct <*> parseDuration <*> parseDynamic <*> parseAccent <*> parseBool
+parseNote = Note <$> parsePitch <*> parseOct <*> parseDuration <*> parseAccent <*> parseDynamic <*> parseBool
 
 instance FromLily Note  where
   parseLily = mkParseLily parseNote
