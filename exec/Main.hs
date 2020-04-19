103ffe2eee875b659cbffad543f2e03548f54c21
ttitchener <ttitchener@gmail.com>
2020-04-19 16:39:49 -0400

    examples

--- exec/Main.hs
+++ exec/Main.hs
@@ -7,10 +7,21 @@ import Control.Monad.Trans.Reader
 import Control.Monad.Trans.State
 
 import Driver
+import Lily
 import Types
 
 main :: IO ()
-main =  liftIO $ execStateT (runReaderT (runDriver example) initEnv) initState >>= print . show
+main =  liftIO $ execStateT (runReaderT (runDriver exComb) initEnv) initState >>= print . show
 
-example :: Driver ()
-example = liftF $ DoAction (WriteLily (Note C COct QDur Accent Forte False)) ()
+printLily :: ToLily a => a -> Driver ()
+printLily l = liftF $ DoAction (WriteLily l) ()
+
+randomize :: ToLily a => [a] -> Driver [a]
+randomize ls = liftF $ DoActionThen (RandomizeList ls) id
+
+exAction :: Driver ()
+exAction = printLily (Note C COct QDur Accent Forte False)
+
+exComb :: Driver ()
+exComb = randomize [C,D,E,F,G,A,B] >>= mapM_ printLily
+
