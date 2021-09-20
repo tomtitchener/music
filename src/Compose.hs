e5bccef97cc9fa1c8cd3b40d3bd8bcd66f998657
ttitchener <ttitchener@gmail.com>
2021-09-20 15:27:06 -0400

    wip

--- src/Compose.hs
+++ src/Compose.hs
@@ -1,19 +1,20 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-
 
 module Compose {--(cfg2Driver, genSwirl)--} where
 
 import Control.Monad (zipWithM)
 import Data.Foldable (fold)
+import Data.Bifunctor (bimap)
 import Data.Functor ((<&>))
-import Data.List (zipWith4)
+import Data.List (zipWith4, groupBy)
 import qualified Data.List.NonEmpty as NE
 import Data.Semigroup (stimesMonoid)
 import Data.Tuple (swap)
+import Data.Tuple.Extra (dupe, first)
 import GHC.Base (sconcat)
-import Safe (headMay)
 
 import Driver
     ( getConfigParam,
@@ -363,6 +364,7 @@ cfg2SwirlsTup pre =
 cfg2SwirlsTups :: String -> NE.NonEmpty String -> Driver (NE.NonEmpty SwirlsTup)
 cfg2SwirlsTups = cfg2Tups cfg2SwirlsTup
     
+{--
 swirlsTup2VEs :: SwirlsTup -> Driver (NE.NonEmpty VoiceEvent)
 swirlsTup2VEs SwirlsTup{..} = 
   genSwirl _stDurs _stPitss _stScale (Range _stRange) <&> NE.map VeNote
@@ -384,29 +386,170 @@ pickInitialClef :: NE.NonEmpty VoiceEvent -> Clef
 pickInitialClef ves =  maybe err pickVoiceEventClef . headMay . NE.filter veIsVeNote $ ves
   where err = error $ "pickInitialClef no VeNote in [VoiceEvent]: " <> show (NE.toList ves)
 
+ve2MaybeClef :: VoiceEvent -> Maybe Clef
+ve2MaybeClef (VeNote note) = Just (pickNoteClef note)
+ve2MaybeClef _ = Nothing
+
+singleton :: a -> NE.NonEmpty a
+singleton = flip (NE.:|) []
+
+-- tbd: insert debounce before accumulation of next element in NE.NonEmpty VoiceEvent pair.
+-- going to mean replacing NE.NonEmpty wih ordinary list so debouncer can answer []!
+-- nb: given ++ as <> for [], ([a],[b]) <> ([a],[b]) works as expected.
+-- so, (trebleVes <> singleton ve,bassVes <> singleton sp) will work as
+--     (trebleVes,bassVes) <> genPair cache ve
+-- currently fold captures state as simply current Clef, this needs to be agumented
+-- with list of VoiceEvent to debounce, max length e.g. 5
+-- uh oh: more complicated than just using <> to append pairs, as I need to update
+-- active clef as output from debouncer as well -- currently hard-wired to change
+-- immediately
+-- question: is it enough to cache list of VoiceEvent alone, note I can easily compute
+-- Maybe clef per VoiceEvent or if that's too awkward, as [(VoiceEvent,Maybe Clef)]
+-- nb: debounce list needs at least 5 VeNote, list could be longer if we accumulate some
+-- Ve<other> events along the way
+-- so that leaves me with state as (Clef,[(VoiceEvent,Clef)]) and type of debounce as
+-- debounce :: (Clef,[(VoiceEvent,Clef)],([VoiceEvent],[VoiceEvent]))
+--             -> VoiceEvent
+--             -> (Clef,[(VoiceEvent,Clef)],([VoiceEvent],[VoiceEvent]))
+-- or b -> a -> b for the foldl.
+-- don't forget:  thread all VoiceEvent instances through cache to avoid out-of-order
+-- don't forget:  purge [VoiceEvent,Clef]
 splitStaves :: Clef -> NE.NonEmpty VoiceEvent -> (NE.NonEmpty VoiceEvent,NE.NonEmpty VoiceEvent)
-splitStaves clef = snd . foldl f (clef,(VeClef Treble NE.:| [],VeClef Bass NE.:| []))
+splitStaves clef = snd . foldl f (clef,(singleton (VeClef Treble),singleton (VeClef Bass)))
   where
     f :: (Clef,(NE.NonEmpty VoiceEvent,NE.NonEmpty VoiceEvent)) -> VoiceEvent -> (Clef,(NE.NonEmpty VoiceEvent,NE.NonEmpty VoiceEvent))
     f (cl,(trebleVes,bassVes)) ve@(VeNote note@Note{..})
-      | cl == Treble && cl' == Treble = (Treble, (trebleVes <> (ve NE.:| []), bassVes <> (sp NE.:| [])))
-      | cl == Bass   && cl' == Treble = (Treble, (trebleVes <> (ve NE.:| []), bassVes <> (sp NE.:| [])))
-      | cl == Bass   && cl' == Bass   = (Bass,   (trebleVes <> (sp NE.:| []), bassVes <> (ve NE.:| [])))
-      | cl == Treble && cl' == Bass   = (Bass,   (trebleVes <> (sp NE.:| []), bassVes <> (ve NE.:| [])))
+      | cl == Treble && cl' == Treble = (Treble, (trebleVes <> singleton ve, bassVes <> singleton sp))
+      | cl == Bass   && cl' == Treble = (Treble, (trebleVes <> singleton ve, bassVes <> singleton sp))
+      | cl == Bass   && cl' == Bass   = (Bass,   (trebleVes <> singleton sp, bassVes <> singleton ve))
+      | cl == Treble && cl' == Bass   = (Bass,   (trebleVes <> singleton sp, bassVes <> singleton ve))
       | otherwise = error $ "splitStaves unexpected clefs: " <> show cl <> " and " <> show cl'
       where
         cl' = pickNoteClef note
         sp = VeSpacer (Spacer _noteDur NoDynamic)
     f (cl,(trebleVes,bassVes)) ve@(VeRest Rest{..})
-      | cl == Treble = (Treble, (trebleVes <> (ve NE.:| []), bassVes <> (sp NE.:| [])))
-      | cl == Bass   = (Bass,   (trebleVes <> (sp NE.:| []), bassVes <> (ve NE.:| [])))
-      | otherwise = error $ "splitSaves unexpected clef: " <> show cl
+      | cl == Treble = (Treble, (trebleVes <> singleton ve, bassVes <> singleton sp))
+      | cl == Bass   = (Bass,   (trebleVes <> singleton sp, bassVes <> singleton ve))
+      | otherwise = error $ "splitStaves unexpected clef: " <> show cl
       where
         sp = VeSpacer (Spacer _restDur NoDynamic)
-    f (cl,(trebleVes,bassVes)) ve@VeTempo {}                 = (cl,(trebleVes <> (ve NE.:| []),bassVes <> (ve NE.:| [])))
-    f (cl,(trebleVes,bassVes)) ve@VeKeySignature {}          = (cl,(trebleVes <> (ve NE.:| []),bassVes <> (ve NE.:| [])))
-    f (cl,(trebleVes,bassVes)) ve@VeTimeSignature {}         = (cl,(trebleVes <> (ve NE.:| []),bassVes <> (ve NE.:| [])))
+    f (cl,(trebleVes,bassVes)) ve@VeTempo {}                 = (cl,(trebleVes <> singleton ve,bassVes <> singleton ve))
+    f (cl,(trebleVes,bassVes)) ve@VeKeySignature {}          = (cl,(trebleVes <> singleton ve,bassVes <> singleton ve))
+    f (cl,(trebleVes,bassVes)) ve@VeTimeSignature {}         = (cl,(trebleVes <> singleton ve,bassVes <> singleton ve))
+    f (_,_) ve = error $ "splitVoiceEvent unexpected VoiceEvent: " <> show ve
+    
+splitVoiceEvent :: Clef -> VoiceEvent -> (Clef,(VoiceEvent,VoiceEvent))
+splitVoiceEvent cl ve@(VeNote note@Note{..})
+      | cl == Treble && cl' == Treble = (Treble, (ve, sp))
+      | cl == Bass   && cl' == Treble = (Treble, (ve, sp))
+      | cl == Bass   && cl' == Bass   = (Bass,   (sp, ve))
+      | cl == Treble && cl' == Bass   = (Bass,   (sp, ve))
+      | otherwise = error $ "splitVoiceEvent unexpected clefs: " <> show cl <> " and " <> show cl'
+      where
+        cl' = pickNoteClef note
+        sp = VeSpacer (Spacer _noteDur NoDynamic)
+splitVoiceEvent cl ve@(VeRest Rest{..})
+      | cl == Treble = (Treble, (ve, sp))
+      | cl == Bass   = (Bass,   (sp, ve))
+      | otherwise = error $ "splitVoiceEventunexpected clef: " <> show cl
+      where
+        sp = VeSpacer (Spacer _restDur NoDynamic)
+splitVoiceEvent cl ve@VeTempo {}                 = (cl,(ve,ve))
+splitVoiceEvent cl ve@VeKeySignature {}          = (cl,(ve,ve))
+splitVoiceEvent cl ve@VeTimeSignature {}         = (cl,(ve,ve))
+splitVoiceEvent _ ve = error $ "splitVoiceEvent  unexpected VoiceEvent: " <> show ve
+
+forceClef :: Clef -> VoiceEvent -> (VoiceEvent,VoiceEvent)
+forceClef cl ve@(VeNote note@Note{..})
+      | cl == Treble = (ve, sp)
+      | cl == Bass   = (sp, ve)
+      | otherwise = error $ "forceClef unexpected clef: " <> show cl
+      where
+        sp = VeSpacer (Spacer _noteDur NoDynamic)
+forceClef cl ve@(VeRest Rest{..})
+      | cl == Treble = (ve, sp)
+      | cl == Bass   = (sp, ve)
+      | otherwise = error $ "forceClef unexpected clef: " <> show cl
+      where
+        sp = VeSpacer (Spacer _restDur NoDynamic)
+forceClef cl ve@VeTempo {}                 = (ve,ve)
+forceClef cl ve@VeKeySignature {}          = (ve,ve)
+forceClef cl ve@VeTimeSignature {}         = (ve,ve)
+forceClef _ ve = error $ "forceClef  unexpected VoiceEvent: " <> show ve
+
+-- algorithm:  first, need to fill window, or can I just grab until first 5 note events to initialize ahead of time?
+-- or do I do it opportunistically:
+-- a) if window is empty, pass through until first note, then take clef of first note and compare with active clef:
+--    a') if clefs are the same, then emit note to output VesPr
+--    a'') if clefs are different, then add note to win
+-- or I can circumvent this whole startup by figuring out the starting clef ahead of time and passing everything
+-- through to VesPr until I encounter the first note with a new clef
+-- 
+type VeClefPr = (VoiceEvent,Clef)
+type VesPr = ([VoiceEvent],[VoiceEvent])
+type SplitState = (Clef,[VeClefPr],VesPr)
+
+splitStaves' :: Clef -> NE.NonEmpty VoiceEvent -> (NE.NonEmpty VoiceEvent,NE.NonEmpty VoiceEvent)
+splitStaves' clef = purge . foldl f (clef,([VeClef Treble],[VeClef Bass])) . bimap NE.toList NE.toList
+  where
+    f :: SplitState -> VoiceEvent -> SplitState
+    f (cl,window,vesPr) ve@(VeNote note@Note{..})
+      | null window && cl == cl' && cl == Treble = (Treble, [], vesPr <> (ve,sp))
+      | null window && cl == cl' && cl == Bass   = (Bass,   [], vesPr <> (sp,ve))
+      | cntNotes < maxRunCnt = (cl,window <> [(cl',ve)],vesPr)
+      | otherwise = (cl',[(cl',ve)],vesPr <> debounce window)
+      where
+        cl' = pickNoteClef note
+        sp = VeSpacer (Spacer _noteDur NoDynamic)
+        maxRunCnt :: Int = 5
+        cntNotes = length (filter isNote window)
+        isNote :: VeClefPr -> Bool
+        isNote (VeNote {},_) = True
+        isNote _ = False
+        debounce win
+          | all (== cl) (map snd notes) = forceClef cl (map fst window)
+          | otherwise = forceClef (otherClef cl) (map fst window)
+          where
+            notes = filter isNote window
+            cl = snd $ head notes
+            forceClef Treble window = map f window
+            otherClef Treble = Bass
+            otherClef Bass = Treble
+            otherClef cl = error $ "otherClef unexpected clef: " <> show cl
+    f (cl,(trebleVes,bassVes)) ve@(VeRest Rest{..})
+      | ...
+    f (cl,(trebleVes,bassVes)) ve@VeTempo {}                 = (cl,(trebleVes <> singleton ve,bassVes <> singleton ve))
+    f (cl,(trebleVes,bassVes)) ve@VeKeySignature {}          = (cl,(trebleVes <> singleton ve,bassVes <> singleton ve))
+    f (cl,(trebleVes,bassVes)) ve@VeTimeSignature {}         = (cl,(trebleVes <> singleton ve,bassVes <> singleton ve))
     f (_,_) ve = error $ "splitStaves unexpected VoiceEvent: " <> show ve
+    purge :: SplitState -> VesPr
+    purge (_, win, ves) = ves <> unzip (map win2Ves win)
+    win2Ves :: VeClefPr -> VesPr
+    win2Ves (Treble,ve) = (ve,ve2Sp ve)
+    win2Ves (Bass,  ve) = (ve2Sp ve,ve)
+    ve2Sp :: VoiceEvent -> Spacer
+    ve2Sp VeNote{..} = Spacer _noteDur NoDynamic
+    ve2Sp VeRest{..} = Spacer _restDur NoDynamic
+    ve2Sp ve = ve
+
+debounceStaves :: (NE.NonEmpty VoiceEvent,NE.NonEmpty VoiceEvent) -> (NE.NonEmpty VoiceEvent,NE.NonEmpty VoiceEvent)
+debounceStaves = bimap NE.fromList NE.fromList . uncurry g . bimap winlist winlist
+  where
+    winlist = windows 5 . NE.toList
+    windows n xs = transpose (take n (tails xs))
+    g tves bves = unzip $ zipWith h tves bves
+    h tves bves
+      | isSpacer t = if any isNoteOrRest (tail tves) then (b,t) else (t,b)
+      | isSpacer b = if any isNoteOrRest (tail bves) then (t,b) else (b,t)
+      | otherwise = (t,b)
+        where
+          t = head tves
+          b = head bves
+          isSpacer VeSpacer {} = True
+          isSpacer _ = False
+          isNoteOrRest VeNote {} = True
+          isNoteOrRest VeRest {} = True
+          isNoteOrRest _ = False
 
 genPolyVocs :: Instrument -> KeySignature -> (NE.NonEmpty VoiceEvent,NE.NonEmpty VoiceEvent) -> Voice
 genPolyVocs instr keySig (treble,bass) = PolyVoice instr vess
@@ -419,7 +562,92 @@ cfg2SwirlsScore title = do
   tups <- cfg2SwirlsTups title (NE.fromList ["voice1"])
   vess <- traverse swirlsTup2VEs tups 
   let clefs = pickInitialClef <$> vess
-      vePrss = NE.zipWith splitStaves clefs vess
+      vePrss = NE.map debounceStaves $ NE.zipWith splitStaves clefs vess
       voices = neZipWith3 genPolyVocs (_stInstr <$> tups) (_stKey <$> tups) vePrss
   writeScore ("./" <> title <> ".ly") $ Score "no comment" voices
+--}
 
+pickNoteClef :: Note -> Clef
+pickNoteClef Note{..}
+  | (_noteOct,_notePit) <= (COct,E) = Bass
+  | otherwise = Treble
+
+swirlsTup2Notes :: SwirlsTup -> Driver [Note]
+swirlsTup2Notes SwirlsTup{..} = 
+  genSwirl _stDurs _stPitss _stScale (Range _stRange) <&> NE.toList
+
+genPolyVocs :: Instrument -> KeySignature -> (NE.NonEmpty VoiceEvent,NE.NonEmpty VoiceEvent) -> Voice
+genPolyVocs instr keySig (treble,bass) = PolyVoice instr vess
+  where
+    vess = NE.fromList [veKeySig NE.<| treble, veKeySig NE.<| bass]
+    veKeySig = VeKeySignature keySig
+
+cfg2SwirlsScore :: String -> Driver ()
+cfg2SwirlsScore title = do
+  tups <- cfg2SwirlsTups title (NE.fromList ["voice1"])
+  notess <- traverse swirlsTup2Notes tups <&> NE.toList
+  let bNotePrss :: [[(Bool,Note)]] = map (map (first ((Treble ==) . pickNoteClef) . dupe)) notess
+      startClefs :: [Bool] = map (fst . head) bNotePrss
+      vePrss = zipWith zipf startClefs bNotePrss
+      zipf :: Bool -> [(Bool,Note)] -> ([VoiceEvent],[VoiceEvent])
+      zipf b bNotePrs = purgeWin $ foldl foldf ((b,[]),([VeClef Treble],[VeClef Bass])) bNotePrs
+      foldf :: ((Bool,[(Bool,Note)]),([VoiceEvent],[VoiceEvent])) -> (Bool,Note) -> ((Bool,[(Bool,Note)]),([VoiceEvent],[VoiceEvent]))
+      foldf ((cl,win),(treb,bass)) (cl',not') =
+        if cl == cl' && null win
+        then ((cl,win),(treb,bass) <> bNote2VePr (cl,not'))
+        else
+          let win' = win <> [(cl',not')]
+          in
+            if length win' < 5
+            then ((cl,win'),(treb,bass))
+            else
+              let wins = groupBy (\(a,_) (b,_) -> a == b)  win'
+                  cl'' = if length (head wins) == 5 then not cl else cl
+              in
+                ((cl'',concat $ tail wins),mconcat ((treb,bass):map bNote2VePr (head wins)))
+      bNote2VePr :: (Bool,Note) -> ([VoiceEvent],[VoiceEvent])
+      bNote2VePr (True,note@Note{..})  = ([VeNote note],[VeSpacer (Spacer _noteDur NoDynamic)])
+      bNote2VePr (False,note@Note{..}) = ([VeSpacer (Spacer _noteDur NoDynamic)],[VeNote note])
+      purgeWin :: ((Bool,[(Bool,Note)]),([VoiceEvent],[VoiceEvent])) -> ([VoiceEvent],[VoiceEvent])
+      purgeWin ((cl,win),(treb,bass)) = mconcat ((treb,bass):(map bNote2VePr (map ((cl,) . snd)  win)))
+      
+      vePrss' = NE.fromList (map (bimap NE.fromList NE.fromList) vePrss)
+      voices = neZipWith3 genPolyVocs (_stInstr <$> tups) (_stKey <$> tups) vePrss'
+  writeScore ("./" <> title <> ".ly") $ Score "no comment" voices
+
+-- Essence of transformation.
+-- 1) simplify swirlsTup2VEs so instead of being
+--   SwirlsTup -> Driver (NE.NonEmpty VoiceEvent)
+-- instead, it's:
+--   SwirlsTup -> Driver (NE.NonEmpty Note)
+-- (what follows is pure and should be pipeline-able)
+-- 2) pair up with a Bool to say if it's treble (True) or bass:
+--   NE.NonEmpty Note -> NE.NonEmpty (Bool, Note)
+-- 3) convert to an ordinary list:
+--   NE.NonEmpty (Bool,Note) -> [(Bool,Note)]
+-- 4) foldl over into a list of pairs ordered treble, bass
+--   [(Bool,Note)] -> [(VoiceEvent,VoiceEvent)]
+--   for state/ouput, foldl takes ((Bool,[(Bool,Note)]),[(VoiceEvent,VoiceEvent)]),
+--   where (Bool,[(Bool,Note)]) is debounce state, [(VoiceEvent,VoiceEvent)] is
+--   output, and input is (Bool,[(Bool,Note)]) and Bool in first part of state indicates
+--   current clef initialized from first Bool in [(Bool,Note)] input.
+--   if input Bool is the same as debounce Bool
+--   then
+--     convert input (Bool,Note) to (NoteEvent,NoteEvent) and append to output
+--   else
+--     append (Bool,Note) to debounce list
+--     if length of debounce list is < 5
+--     then
+--       iterate
+--     else
+--       group debounce list by Bool
+--       if length of first element in [[]] is 5
+--       then
+--         iterate with debounce state with flipped bool
+--           and empty list and all debounce list elements
+--           converted and appended to output
+--       else
+--         iterate with existing debounce state Bool,
+--           and concatenation of tail of [[]] for list
+--           and convert head of [[]] to output flipping
+--           all Bool in (Bool,Note) before converting
